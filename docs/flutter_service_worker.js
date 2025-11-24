567198da89fbc6c90dee5c63827221cd5de4d858
iehgmli71-tech <iehgmli71@gamil.com>
2025-11-24 15:02:20 +0900

    rebuild flutter web without web-renderer flag

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b120db4f7f93c3e51db17980147e6a46
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0c5a9cc84d55713ec2f2467b4164d1d3",
+"flutter_bootstrap.js": "9a5f4c29dc17e7ec937b24e63d60693a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

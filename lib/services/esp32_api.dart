1e3f5e02003f5468026ff0426421037d84d849e7
hyu70664-ux <hyu70664@gamil.com>
2025-12-06 16:01:26 +0900

    esp32 WIFI 주소 값 변경

--- lib/services/esp32_api.dart
+++ lib/services/esp32_api.dart
@@ -2,9 +2,9 @@ import 'package:http/http.dart' as http;
 
 class Esp32Api {
   // ===== 1) 각 보드 IP =====
-  static const String washerBaseUrl = 'http://192.168.219.229';  // 세탁기
-  static const String fridgeBaseUrl = 'http://192.168.219.53';  // 냉장고
-  static const String acBaseUrl     = 'http://192.168.219.197';  // 에어컨
+  static const String washerBaseUrl = 'http://172.20.10.3';  // 세탁기
+  static const String fridgeBaseUrl = 'http://172.20.10.4';  // 냉장고
+  static const String acBaseUrl     = 'http://172.20.10.5';  // 에어컨
 
   // ===== 1-1) 가전 전원 상태 (켜짐 여부) 기억 =====
   //  - true  : 해당 가전이 "켜져 있음"
